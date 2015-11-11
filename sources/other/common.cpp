bf0ae0dda46262bea3687557b9aa87805acfefc3
Dan Sebastian Thrane <dthrane@gmail.com>
2015-11-11 14:08:52 +0100

    Added asserts for visited particles

--- sources/other/common.cpp
+++ sources/other/common.cpp
@@ -10,15 +10,6 @@
 
 double size;
 
-//
-//  tuned constants
-//
-#define density 0.0005
-#define mass    0.01
-#define cutoff  0.01
-#define min_r   (cutoff/100)
-#define dt      0.0005
-
 static bool initialized = false;
 static bool first = true;
 
--- sources/other/serial.cpp
+++ sources/other/serial.cpp
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#include <algorithm>
+#include <assert.h>
 
 #include "common.h"
 #include "grid.h"
@@ -9,6 +11,10 @@
 // benchmarking program
 // note: i don't expect this to compile.
 int runSerialImplementation(int argc, char **argv) {
+#ifdef DEBUG
+    printf("This is a debug build!\n");
+#endif
+
     if (find_option(argc, argv, "-h") >= 0) {
         printf("Options:\n");
         printf("-h to see this help\n");
@@ -29,6 +35,21 @@ int runSerialImplementation(int argc, char **argv) {
     return runSerialWithParticles(fsave, n, particles);
 }
 
+std::vector<particle_t *> find_all_colliding_particles(int n, particle_t *particles, const particle_t &particle) {
+    std::vector<particle_t *> result;
+    for (int i = 0; i < n; i++) {
+        particle_t *neighbor = &particles[i];
+
+        double dx = neighbor->x - particle.x;
+        double dy = neighbor->y - particle.y;
+        double r2 = dx * dx + dy * dy;
+        if (r2 <= cutoff * cutoff) {
+            result.push_back(neighbor);
+        }
+    }
+    return result;
+}
+
 int runSerialWithParticles(FILE *fsave, int n, particle_t *particles) {
     // new additions for linear runstime
     grid_init(sqrt(0.0005 * n) / 0.01);
@@ -39,7 +60,9 @@ int runSerialWithParticles(FILE *fsave, int n, particle_t *particles) {
     //  simulate a number of time steps
     double simulation_time = read_timer();
     for (int step = 0; step < NSTEPS; step++) {
+#ifdef DEBUG
         validate_grid(n);
+#endif
         printf("NSTEP = %i\n", step);
 
         //  compute forces
@@ -47,6 +70,9 @@ int runSerialWithParticles(FILE *fsave, int n, particle_t *particles) {
             particles[i].ax = particles[i].ay = 0;
 
             // traverse included neighbors
+#ifdef DEBUG
+            std::vector<particle_t *> all_particles_visited;
+#endif
             for (int offsetX = -1; offsetX <= 1; offsetX++) {
                 for (int offsetY = -1; offsetY <= 1; offsetY++) {
                     const std::vector<particle_t *> &cell =
@@ -54,9 +80,25 @@ int runSerialWithParticles(FILE *fsave, int n, particle_t *particles) {
 
                     for (auto particle : cell) {
                         apply_force(particles[i], *particle);
+#ifdef DEBUG
+                        all_particles_visited.push_back(particle);
+#endif
                     }
                 }
             }
+
+#ifdef DEBUG
+            auto all = find_all_colliding_particles(n, particles, particles[i]);
+            assert(all.size() == all_particles_visited.size());
+            for (auto particle : all) {
+                if (std::find(all_particles_visited.begin(), all_particles_visited.end(),
+                              particle) == all_particles_visited.end()) {
+                    printf("expected cell (%lf,%lf) to collide with (%lf,%lf) in iteration %i, it did not.\n",
+                           particle->x, particle->y, particles[i].x, particles[i].y, i);
+                    assert(false);
+                }
+            }
+#endif
         }
 
         //  move particles
